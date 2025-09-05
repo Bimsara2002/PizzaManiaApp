c6abb33fe729176d2af8bff13e50aa60b0df0e06
K.A Bimsara Kaushal <bimsarakaushal2002@gmail.com>
2025-09-06 00:28:30 +0530

    initial commit

---
+++ app/src/main/java/com/example/pizzamaniaapp/DBHelper.java
@@ -0,0 +1,51 @@
+package com.example.pizzamaniaapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+import android.database.Cursor;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DBNAME = "PizzaMania.db";
+    public Cursor getAllUsers() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM users", null);
+    }
+
+    public DBHelper(Context context) {
+        super(context, DBNAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDB) {
+        MyDB.execSQL("create table users(username TEXT primary key, password TEXT, mobile TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
+        MyDB.execSQL("drop table if exists users");
+    }
+
+    public Boolean insertData(String username, String password, String mobile) {
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("username", username);
+        values.put("password", password);
+        values.put("mobile", mobile);
+        long result = MyDB.insert("users", null, values);
+        return result != -1;
+    }
+
+    public Boolean checkUsername(String username) {
+        SQLiteDatabase MyDB = this.getReadableDatabase();
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ?", new String[]{username});
+        return cursor.getCount() > 0;
+    }
+
+    public Boolean checkUsernamePassword(String username, String password) {
+        SQLiteDatabase MyDB = this.getReadableDatabase();
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ? and password = ?", new String[]{username, password});
+        return cursor.getCount() > 0;
+    }
+}
